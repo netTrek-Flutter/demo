a02f636930024b89333ac4e785023817d3c21782
Nik Benson <NikBenson@pm.me>
2021-11-19 11:37:31 +0100

    updated bloc syntax

--- lib/bloc/bloc.dart
+++ lib/bloc/bloc.dart
@@ -4,8 +4,15 @@ import 'events.dart';
 import 'states.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  ThemeBloc() : super(LightThemeState());
+  ThemeBloc() : super(LightThemeState()) {
+    on<LightThemeEvent>((event, emit) => emit(LightThemeState()));
+    on<DarkThemeEvent>((event, emit) => emit(DarkThemeState()));
+    on<ToggleThemeEvent>((event, emit) => emit(state is LightThemeState? DarkThemeState() : LightThemeState()));
+    on<CustomThemeEvent>((event, emit) => emit(CustomThemeState(event.theme)));
+  }
 
+/*
+  /// Deprecated
   @override
   Stream<ThemeState> mapEventToState(ThemeEvent event) async* {
     if (event is LightThemeEvent)
@@ -16,4 +23,5 @@ class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
       yield (state is LightThemeState) ? DarkThemeState() : LightThemeState();
     else if (event is CustomThemeEvent) yield CustomThemeState(event.theme);
   }
+  */
 }
